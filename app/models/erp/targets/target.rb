7b68a9dbf5bc43871595fb6a6111dfb523436c8e
Luan Pham <luanpm88@gmail.com>
2017-11-30 09:47:26 +0700

    fix target

--- app/models/erp/targets/target.rb
+++ app/models/erp/targets/target.rb
@@ -3,29 +3,33 @@ module Erp::Targets
     belongs_to :creator, class_name: 'Erp::User'
     belongs_to :salesperson, class_name: 'Erp::User'
     belongs_to :period, class_name: 'Erp::Periods::Period'
-    
+
     has_many :target_details, inverse_of: :target, dependent: :destroy
     accepts_nested_attributes_for :target_details, :reject_if => lambda { |a| a[:percent].blank? || a[:commission_amount].blank? }, :allow_destroy => true
-    
+
     # class const
     STATUS_ACTIVE = 'active'
     STATUS_DELETED = 'deleted'
-    
+
+    def amount=(new_price)
+      self[:amount] = new_price.to_s.gsub(/\,/, '')
+    end
+
     # Filters
     def self.filter(query, params)
       params = params.to_unsafe_hash
-      
+
       # join with users table for search creator
       query = query.joins(:creator)
-      
+
       # join with users table for search salesperson
       query = query.joins(:salesperson)
-      
+
       # join with users table for search period
       query = query.joins(:period)
-      
+
       and_conds = []
-      
+
       #filters
       if params["filters"].present?
         params["filters"].each do |ft|
@@ -36,7 +40,7 @@ module Erp::Targets
           and_conds << '('+or_conds.join(' OR ')+')' if !or_conds.empty?
         end
       end
-      
+
       #keywords
       if params["keywords"].present?
         params["keywords"].each do |kw|
@@ -47,34 +51,34 @@ module Erp::Targets
           and_conds << '('+or_conds.join(' OR ')+')'
         end
       end
-      
+
       # add conditions to query
       query = query.where(and_conds.join(' AND ')) if !and_conds.empty?
-      
+
       # global filters
       global_filter = params[:global_filter]
 
       if global_filter.present?
-				
+
 				# filter by destination warehouse
 				if global_filter[:period].present?
 					query = query.where(period_id: global_filter[:period])
 				end
-				
+
 				# filter by destination warehouse
 				if global_filter[:salesperson].present?
 					query = query.where(salesperson_id: global_filter[:salesperson])
 				end
-				
+
 			end
-      
+
       return query
     end
-    
+
     def self.search(params)
       query = self.all
       query = self.filter(query, params)
-      
+
       # order
       if params[:sort_by].present?
         order = params[:sort_by]
@@ -84,41 +88,41 @@ module Erp::Targets
       else
 				query = query.order('created_at desc')
       end
-      
+
       return query
     end
-    
+
     def set_active
       update_columns(status: Erp::Targets::Target::STATUS_ACTIVE)
     end
-    
+
     def set_deleted
       update_columns(status: Erp::Targets::Target::STATUS_DELETED)
     end
-    
+
     def self.set_active_all
       update_all(status: Erp::Targets::Target::STATUS_ACTIVE)
     end
-    
+
     def self.set_deleted_all
       update_all(status: Erp::Targets::Target::STATUS_DELETED)
     end
-    
+
     def is_active?
       return self.status == Erp::Targets::Target::STATUS_ACTIVE
     end
-    
+
     def is_deleted?
       return self.status == Erp::Targets::Target::STATUS_DELETED
     end
-    
+
     def salesperson_name
 			salesperson.present? ? salesperson.name : ''
 		end
-    
+
     def period_name
 			period.present? ? period.name : ''
 		end
-    
+
   end
 end
--- app/models/erp/targets/target_detail.rb
+++ app/models/erp/targets/target_detail.rb
@@ -1,5 +1,13 @@
 module Erp::Targets
   class TargetDetail < ApplicationRecord
     belongs_to :target, class_name: 'Erp::Targets::Target'
+
+    def commission_amount=(new_price)
+      self[:commission_amount] = new_price.to_s.gsub(/\,/, '')
+    end
+
+    def percent=(new_price)
+      self[:percent] = new_price.to_s.gsub(/\,/, '')
+    end
   end
 end
